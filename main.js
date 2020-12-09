1387e916d725e760b7fe6554dbf26a26dab34f83
Midgardsormr201 <diego.guerradi@sansano.usm.cl>
2020-12-09 00:20:46 -0300

    Nueva pagina def

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-const map = L.map('map-template').setView([-33.437280, -70.634356],17);
-
-
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-
+var map = L.map('map').setView([-33.437280, -70.634356],17);
+//Función geolocalizador
 var lc = L.control.locate({
-    position: 'topleft',
-    setView: 'always',
-    strings: {
-          title: "Muestrame donde estoy!"
-        }
-    }).addTo(map);
-
-var c = new L.Control.Coordinates();
-    c.addTo(map);
-    function onMapClick(e) {
-      c.setCoordinates(e);
+initialZoomLevel: 14,
+position: 'topleft',
+setView: 'always',
+strings: {
+      title: "Muestrame donde estoy!"
     }
-    map.on('click', onMapClick);    
+}).addTo(map);
+//Función geolocalizador
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: 'Map data &copy; <a href="http://openstreetmap.org/%22%3EOpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/%22%3ECC-BY-SA</a>, Imagery © <a href="http://cloudmade.com/%22%3ECloudMade</a>',
+maxZoom: 18
+}).addTo(map);
+
+//Función coordenadas
+var c = new L.Control.Coordinates();
+c.addTo(map);
+function onMapClick(e) {
+  c.setCoordinates(e);
+}
+map.on('click', onMapClick);
+//Función coordenadas
+
+//Función buscador
+var osmGeocoder = new L.Control.OSMGeocoder({
+placeholder: 'Buscar Ubicación',
+text: 'Buscar',
+collapsed: true,
+position: 'topright',
+});
+map.addControl(osmGeocoder);
